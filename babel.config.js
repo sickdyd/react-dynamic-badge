d97959cc49248881ca1ee80f790b4f4602973dd6
sickdyd <roberto.reale.ja@gmail.com>
2020-01-12 12:14:23 +0900

    Removed tests from npm package

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,10 @@ module.exports = function (api) {
 	api.cache(true);
 
 	const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-	const plugins = [ ];
+  const plugins = [ ];
 
 	return {
 		presets,
-		plugins
+    plugins,
 	};
 };
