fed6b3b399dee3f9714b838c4c4b8b9070009e54
Rob <roberto.reale.ja@gmail.com>
2020-12-31 21:51:12 +0900

    Prettify files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/src/test/__mocks__/styleMock.js',
-    "setupFiles": [
-      "./__mocks__/client.js"
-    ],
+    'setupFiles': ['./__mocks__/client.js']
   }
-};
+}
